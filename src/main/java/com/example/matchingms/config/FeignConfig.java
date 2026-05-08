56ec812be9613f3ad69622f209f7d2e1ca3f6604
Farid Khalilov <009470@ibar.int>
2026-05-08 09:50:45 +0400

    fixed user-info-ms url in feign

--- src/main/java/com/example/matchingms/config/FeignConfig.java
+++ src/main/java/com/example/matchingms/config/FeignConfig.java
@@ -1,4 +1,12 @@
 package com.example.matchingms.config;
 
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+
 public class FeignConfig {
+
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default(100, 1000, 3);
+    }
 }
